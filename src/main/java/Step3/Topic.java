73c825b1952c345a9f1f65d5bf432b954b015d5b
H-Orkhan <orxan.hashimov2014@gmail.com>
2020-02-04 01:20:58 +0400

    ** Step 3 **

--- src/main/java/Step3/Topic.java
+++ src/main/java/Step3/Topic.java
@@ -9,24 +9,14 @@ public class Topic {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public Topic() { }
