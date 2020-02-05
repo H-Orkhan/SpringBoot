8694aac1a3b58a889d9a853db8dd02aaaf86dca6
H-Orkhan <orxan.hashimov2014@gmail.com>
2020-02-05 17:16:54 +0400

    ** Added Step 1 **

--- src/main/java/Step1/SpringApp.java
+++ src/main/java/Step1/SpringApp.java
@@ -2,9 +2,19 @@ package Step1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringApp {
+public class SpringApp extends SpringBootServletInitializer {
+
+    //We need to extend the class SpringBootServletInitializer
+    //to support WAR file deployment.
+    @Override
+    protected SpringApplicationBuilder configure (
+            SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(SpringApp.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class,args);
     }
